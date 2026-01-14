cb17c801120aa166ce2ae69496c0602d2545764f
Bayar Ganchimeg <bayarbna@gmail.com>
2026-01-14 13:09:36 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+
+    loop = True
+    while loop:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                loop = False
+
+        screen.fill("black")
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+
+
+if __name__ == "__main__":
+    main()
